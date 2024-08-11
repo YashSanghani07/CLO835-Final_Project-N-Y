d5433b4a6b810fb971a38334261e0cb65c389eec
YashSanghani07 <159961522+YashSanghani07@users.noreply.github.com>
2024-08-10 22:57:56 -0400

    Update app.py

--- application/app.py
+++ application/app.py
@@ -5,27 +5,31 @@ import random
 import argparse
 import boto3
 import botocore
+
 app = Flask(__name__)
 
-DBHOST = os.environ.get("DBHOST") or "localhost"
-DBUSER = os.environ.get("DBUSER") or "root"
-DBPWD = os.environ.get("DBPWD") or "passwors"
-DATABASE = os.environ.get("DATABASE") or "employees"
-COLOR_FROM_ENV = os.environ.get('APP_COLOR') or "lime"
+# Environment variables with defaults
+DBHOST = os.environ.get("DBHOST", "localhost")
+DBUSER = os.environ.get("DBUSER", "root")
+DBPWD = os.environ.get("DBPWD", "password")
+DATABASE = os.environ.get("DATABASE", "employees")
 DBPORT = int(os.environ.get("DBPORT", 3306))
-BACKGROUND_IMAGE = os.environ.get("BACKGROUND_IMAGE") or "Invalid Image been passed"
-GROUP_NAME = os.environ.get('GROUP_NAME') or "N&Y"
+COLOR_FROM_ENV = os.environ.get('APP_COLOR', "lime")
+BACKGROUND_IMAGE = os.environ.get("BACKGROUND_IMAGE", "https://mybucketclo835.s3.amazonaws.com/black.jpg")
+GROUP_NAME = os.environ.get('GROUP_NAME', "N&Y")
 
 # Create a connection to the MySQL database
-db_conn = connections.Connection(
-    host= DBHOST,
-    port= DBPORT,
-    user= DBUSER,
-    password= DBPWD,
-    db= DATABASE
-)
-output = {}
-table = 'employee';
+try:
+    db_conn = connections.Connection(
+        host=DBHOST,
+        port=DBPORT,
+        user=DBUSER,
+        password=DBPWD,
+        db=DATABASE
+    )
+except Exception as e:
+    print(f"Error connecting to database: {e}")
+    db_conn = None
 
 # Define the supported color codes
 color_codes = {
@@ -38,7 +42,6 @@ color_codes = {
     "lime": "#C1FF9C",
 }
 
-
 # Create a string of supported colors
 SUPPORTED_COLORS = ",".join(color_codes.keys())
 
@@ -47,42 +50,38 @@ COLOR = random.choice(["red", "green", "blue", "blue2", "darkblue", "pink", "lim
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    print('show me the background image url',BACKGROUND_IMAGE)
+    print('show me the background image url', BACKGROUND_IMAGE)
     image_url = url_for('static', filename='background_image.png')
-    return render_template('addemp.html', background_image = image_url, group_name = GROUP_NAME)
+    return render_template('addemp.html', background_image=image_url, group_name=GROUP_NAME)
+
 @app.route("/download", methods=['GET','POST'])
-  #https://privatebucketclo835.s3.amazonaws.com/minionparty.png
 def download(image_url):
-   try:
-         bucket = image_url.split('//')[1].split('.')[0]
-         object_name = '/'.join(image_url.split('//')[1].split('/')[1:])
-         print(bucket)  # prints 'privatebucketclo835'
-         print(object_name)  # prints 'minionparty.png'
-         print("Background Image Location --->" + image_url) # Added for Logging of Background Image Path
-         s3 = boto3.resource('s3')
-         output_dir = "static"
-         if not os.path.exists(output_dir):
-                 os.makedirs(output_dir)
-         output = os.path.join(output_dir, "background_image.png")
-         s3.Bucket(bucket).download_file(object_name, output)
+    try:
+        bucket = image_url.split('//')[1].split('.')[0]
+        object_name = '/'.join(image_url.split('//')[1].split('/')[1:])
+        print(bucket)  # prints 'privatebucketclo835'
+        print(object_name)  # prints 'minionparty.png'
+        print("Background Image Location --->" + image_url)  # Added for Logging of Background Image Path
+        s3 = boto3.resource('s3')
+        output_dir = "static"
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+        output = os.path.join(output_dir, "background_image.png")
+        s3.Bucket(bucket).download_file(object_name, output)
 
-         return output
+        return output
 
-   except botocore.exceptions.ClientError as e:
-                if e.response['Error']['Code'] == "404":
-                    print("The object does not exist.")
-                else:
-                    raise
-
-
-
-
+    except botocore.exceptions.ClientError as e:
+        if e.response['Error']['Code'] == "404":
+            print("The object does not exist.")
+        else:
+            raise
 
 @app.route("/about", methods=['GET','POST'])
 def about():
     image_url = url_for('static', filename='background_image.png')
-    return render_template('about.html', background_image = image_url, group_name = GROUP_NAME)
-    
+    return render_template('about.html', background_image=image_url, group_name=GROUP_NAME)
+
 @app.route("/addemp", methods=['POST'])
 def AddEmp():
     image_url = url_for('static', filename='background_image.png')
@@ -92,27 +91,23 @@ def AddEmp():
     primary_skill = request.form['primary_skill']
     location = request.form['location']
 
-  
     insert_sql = "INSERT INTO employee VALUES (%s, %s, %s, %s, %s)"
     cursor = db_conn.cursor()
 
     try:
-        
-        cursor.execute(insert_sql,(emp_id, first_name, last_name, primary_skill, location))
+        cursor.execute(insert_sql, (emp_id, first_name, last_name, primary_skill, location))
         db_conn.commit()
-        emp_name = "" + first_name + " " + last_name
-
+        emp_name = f"{first_name} {last_name}"
     finally:
         cursor.close()
 
-    print("all modification done...")
-    return render_template('addempoutput.html', name=emp_name, color=color_codes[COLOR], group_name = GROUP_NAME)
+    print("All modifications done...")
+    return render_template('addempoutput.html', name=emp_name, color=color_codes[COLOR], group_name=GROUP_NAME)
 
 @app.route("/getemp", methods=['GET', 'POST'])
 def GetEmp():
     image_url = url_for('static', filename='background_image.png')
-    return render_template("getemp.html", background_image = image_url, group_name = GROUP_NAME)
-
+    return render_template("getemp.html", background_image=image_url, group_name=GROUP_NAME)
 
 @app.route("/fetchdata", methods=['GET','POST'])
 def FetchData():
@@ -123,24 +118,27 @@ def FetchData():
     cursor = db_conn.cursor()
 
     try:
-        cursor.execute(select_sql,(emp_id))
+        cursor.execute(select_sql, (emp_id,))
         result = cursor.fetchone()
         
-        # Add No Employee found form
-        output["emp_id"] = result[0]
-        output["first_name"] = result[1]
-        output["last_name"] = result[2]
-        output["primary_skills"] = result[3]
-        output["location"] = result[4]
-        
+        if result:
+            output["emp_id"] = result[0]
+            output["first_name"] = result[1]
+            output["last_name"] = result[2]
+            output["primary_skills"] = result[3]
+            output["location"] = result[4]
+        else:
+            print(f"No employee found with ID: {emp_id}")
+            return "No employee found"
+
     except Exception as e:
-        print(e)
+        print(f"Error fetching data: {e}")
 
     finally:
         cursor.close()
 
     return render_template("getempoutput.html", id=output["emp_id"], fname=output["first_name"],
-                           lname=output["last_name"], interest=output["primary_skills"], location=output["location"], color=color_codes[COLOR], group_name = GROUP_NAME)
+                           lname=output["last_name"], interest=output["primary_skills"], location=output["location"], color=color_codes[COLOR], group_name=GROUP_NAME)
 
 if __name__ == '__main__':
     download(BACKGROUND_IMAGE)
@@ -153,9 +151,9 @@ if __name__ == '__main__':
         print("Color from command line argument =" + args.color)
         COLOR = args.color
         if COLOR_FROM_ENV:
-            print("A color was set through environment variable -" + COLOR_FROM_ENV + ". However, color from command line argument takes precendence.")
+            print("A color was set through environment variable -" + COLOR_FROM_ENV + ". However, color from command line argument takes precedence.")
     elif COLOR_FROM_ENV:
-        print("No Command line argument. Color from environment variable =" + COLOR_FROM_ENV)
+        print("No command line argument. Color from environment variable =" + COLOR_FROM_ENV)
         COLOR = COLOR_FROM_ENV
     else:
         print("No command line argument or environment variable. Picking a Random Color =" + COLOR)
@@ -165,4 +163,4 @@ if __name__ == '__main__':
         print("Color not supported. Received '" + COLOR + "' expected one of " + SUPPORTED_COLORS)
         exit(1)
 
-    app.run(host='0.0.0.0',port=81,debug=True)
+    app.run(host='0.0.0.0', port=81, debug=True)
